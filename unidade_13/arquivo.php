823bb6d13d6018fac847f105a972988d3ee59962
Setonde Nougbodohoue <touscool10@gmail.com>
2022-01-12 17:38:06 -0300

    metodo __autoload e funçoes de manipulaçoes de objetos

--- unidade_13/arquivo.php
+++ unidade_13/arquivo.php
@@ -1,3 +1,28 @@
+<?php
+    function __autoload($classe) {
+        include_once("classes/{$classe}.class.php");
+    }
+
+    $minhaCasa  = new Casa();
+    $minhaCasa->rua = "Rua Pedro Silva";
+    $minhaCasa->numero = 155;
+    $minhaCasa->bairro ="Costeira do Pirajubaé"; 
+    $minhaCasa->cidade ="Florianópolis";
+
+    $meuCarro   = new Carro();
+    $eu         = new Pessoa();
+
+    echo "<pre>";echo "<pre>";
+    print_r(get_class_methods(Casa));
+    echo "<br>";
+    print_r(get_class_vars(Casa));
+    echo "<br>";
+    print_r(get_object_vars($minhaCasa));
+    echo "<br>";
+    echo "</pre>";
+?>
+
+
 <!doctype html>
 <html>
     <head>
